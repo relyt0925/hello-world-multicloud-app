832877fd2d53e4308234d43b788f4395c40663d8
Tyler Lisowski <lisowski@us.ibm.com>
2024-03-11 23:32:23 -0500

    up

--- build-stage.sh
+++ build-stage.sh
@@ -8,7 +8,7 @@ export GIT_HEAD_COMMIT=$(git rev-parse HEAD)
 echo "$SIGNING_PRIV_KEY_64" | base64 -d > /tmp/priv.key
 gpg --import /tmp/priv.key
 rm -f /tmp/priv.key
-export FULL_IMAGE_NAME="us.icr.io/tylerlisnamespace/hello-world-multicloud-app:${GIT_HEAD_COMMIT}"
+export PIPELINE_IMAGE_URL="us.icr.io/tylerlisnamespace/hello-world-multicloud-app:${GIT_HEAD_COMMIT}"
 make dockerbuild
 envsubst < deployment/deployment.yaml > deployment/templated-deployment.yaml
 ibmcloud sat config version create --name "$GIT_HEAD_COMMIT" --config hello-world-multicloud-app --file-format yaml --read-config deployment/templated-deployment.yaml
